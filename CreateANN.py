818a2f02c4bf9d38a749473188fff8ef2b0d3476
jay-batavia <bataviajay@gmail.com>
2015-04-13 16:46:22 -0600

    Changed the ANN parameter values

--- CreateANN.py
+++ CreateANN.py
@@ -4,9 +4,9 @@ from pyfann import libfann
 
 connection_rate = 1
 learning_rate = 0.01
-num_input = 29
-num_hidden = 13
-num_output = 1
+num_input = 16
+num_hidden = 15
+num_output = 14
 
 desired_error = 0.0001
 max_iterations = 10000
@@ -17,6 +17,6 @@ ann.create_sparse_array(connection_rate, (num_input, num_hidden, num_output))
 ann.set_learning_rate(learning_rate)
 ann.set_activation_function_output(libfann.SIGMOID_SYMMETRIC_STEPWISE)
 
-ann.train_on_file("Higgs2.data", max_iterations, iterations_between_reports, desired_error)
+ann.train_on_file("Tetris_Training.data", max_iterations, iterations_between_reports, desired_error)
 
-ann.save("Higgs2.net")
+ann.save("TetrisNet.net")
