445cd6b17a3b6216bda4f59da9ed14dbc32f4e2f
jay-batavia <bataviajay@gmail.com>
2015-04-13 17:01:02 -0600

    Fixed runtime error. Changed structure of the arrays. Removed normalization since it wasn't required.

--- makeData.java
+++ makeData.java
@@ -4,109 +4,79 @@ import java.lang.String;
 
 public class makeData{
 	public static ArrayList<ArrayList<Double>> xs = new ArrayList<>();
-	public static ArrayList<Double> ys = new ArrayList<>();
-
-  public static final String OUTPUT_FILE = "Training.data";
+	public static ArrayList<ArrayList<Double>> ys = new ArrayList<>();
 	
 	public makeData(){
 
 	}
 
-	public static void normalize() {
-      Double miny = ys.get(0);
-      for (Double y : ys) {
-         if (y < miny) miny = y;
-      }
-      for (int i=0; i<ys.size(); i++) {
-         Double y = ys.get(i);
-         y -= miny;
-         ys.set(i , y);
-      }
-      Double maxy = ys.get(0);
-      for (Double y : ys) {
-         if (y > maxy) maxy = y;
-      }
-      for (int i=0; i<ys.size(); i++) {
-         Double y = ys.get(i);
-         y = y / maxy;
-         ys.set(i , y);
-      }
-      
-      //now xs
-      for (int c = 0; c < xs.get(0).size(); c++) {
-         Double min = xs.get(0).get(c);
-         for (int i = 0; i < xs.size(); i++) {
-            Double x = xs.get(i).get(c);
-            if (x < min) min = x;
-         }
-         for (int i = 0; i < xs.size(); i++) {
-            Double x = xs.get(i).get(c);
-            x -= min;
-            xs.get(i).set(c, x);
-         }
-         Double max = xs.get(0).get(c);
-         for (int i = 0; i < xs.size(); i++) {
-            Double x = xs.get(i).get(c);
-            if (x > max) max = x;
-         }
-         for (int i = 0; i < xs.size(); i++) {
-            Double x = xs.get(i).get(c);
-            x = x / max;
-            xs.get(i).set(c, x);
-        }         
-     	}
-    }
-
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException{
 
-		Scanner scan = new Scanner(new File(args[0]));
-    	scan.useDelimiter(",");
+		Scanner scan = new Scanner(new File("output.txt"));
+    	//scan.useDelimiter("\\*");
     	while (scan.hasNextLine()) {  
         	String line = (scan.nextLine());
+        	//System.out.println(line);
             if (line.contains((CharSequence)"?")){ continue;}
-            String[] arr = line.split(",");
-            ArrayList<Double> temp = new ArrayList<Double>();
-            for (int i=0; i<arr.length; i++){
-               Double dub;
-               try { dub = Double.parseDouble(arr[i]); }
-               catch (NumberFormatException nfe) { continue; }
-               if (i == 0){
-                   ys.add(dub);
-               }
-               else{   
-                   temp.add(dub);
-               }
+            
+            String[] input_output = line.split("\\*");
+            String input_vector = input_output[0];
+            String output_vector = input_output[1];
+
+            input_vector = input_vector.replaceAll("\\[","").replaceAll("\\]","");
+            output_vector = output_vector.replaceAll("\\[","").replaceAll("\\]",""); 
+
+            String[] xArr = input_vector.split(",");
+            String[] yArr = output_vector.split(",");
+
+            ArrayList<Double> tempx = new ArrayList<>();
+            ArrayList<Double> tempy = new ArrayList<>();
+
+            for(int i = 0; i<xArr.length;i++){
+            	Double dub;
+            	dub = Double.parseDouble(xArr[i]);
+            	tempx.add(dub);
             }
-            xs.add(temp);
-      	} 
+
+            for(int j=0;j<yArr.length;j++){
+            	Double dub;
+            	dub = Double.parseDouble(yArr[j]);
+            	tempy.add(dub);
+            }
+
+            xs.add(tempx);
+            ys.add(tempy);
+
+    	} 
      	scan.close();
 
-     	normalize();
 
-
-
-     	File fout = new File(args[1]);
+     	File fout = new File("Tetris_ANN.data");
      	FileOutputStream fos  = new FileOutputStream(fout);
 
      	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
 
      	int header1 = xs.size();
      	int header2 = xs.get(0).size()-1;
-     	int header3 = 1;
+     	int header3 = ys.get(0).size()-1;
 
      	bw.write(header1+" "+header2+ " "+header3+ "\n");
-     	int count = 0;
-     	for(ArrayList<Double> vector : xs){
-     		for(Double feature : vector){
-	     		bw.write(feature+ " ");
+
+
+     	for(int count=0; count<xs.size();count++){
+     		ArrayList<Double> xvector = xs.get(count);
+     		ArrayList<Double> yvector = ys.get(count);
+     		for(Double feature:xvector){
+     			bw.write(feature+" ");
      		}
-     		bw.write(1.0+"");
      		bw.write("\n");
-     		bw.write(String.valueOf(ys.get(count)));
-     		bw.write("\n");
-     		count++;
+     		for(Double feature:yvector){
+     			bw.write(feature+" ");
+     		}
+     		bw.write("\n");     		
      	}
+     	
      	bw.flush();
 
 	}
